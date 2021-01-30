57eb5b6d721ae2ebdae04a107ed5bac75a116f8f
Mehmet Ali Gavcar <maligavcar1997@gmail.com>
2021-01-30 09:04:44 +0300

    Version Update

---
+++ Lost/Assets/Scripts/Target.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float hitPoint = 50f;
+    public void TakeDamage(float amount)
+    {
+        hitPoint -= amount;
+        if(hitPoint <= 0f)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
+
